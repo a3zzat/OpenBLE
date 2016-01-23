2952a043d10f17ae6d16069995cae263e962d233
a3zzat <viva.amego@gmail.com>
2016-01-23 19:42:13 +0100

    Defining BLE CIDs

--- L2CAP/BLE_L2CAP_public.h
+++ L2CAP/BLE_L2CAP_public.h
@@ -5,3 +5,9 @@
  * General Public License v3.0. or Above See the file LICENSE in the 
  * top level directory for more details.
  */
+ 
+ // L2CAP Fixed CIDs    
+#define L2CAP_CID_ATTRIBUTE_PROTOCOL        0x0004
+#define L2CAP_CID_LE_SIGNALING              0x0005
+#define L2CAP_CID_SECURITY_MANAGER_PROTOCOL 0x0006
+
