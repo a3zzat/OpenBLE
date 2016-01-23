cfe31fb2f1011133ffc541f4598de5440f774694
a3zzat <viva.amego@gmail.com>
2016-01-23 19:27:59 +0100

    adding the copyright header

---
+++ L2CAP/BLE_L2CAP.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2016 Ahmed Ezzat 
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v3.0. or Above See the file LICENSE in the 
+ * top level directory for more details.
+ */
+ 
